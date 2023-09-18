b5ae017d6e97e1b6d3f00a54ecbdf3e2a8891a8d
Katrina Arbogast <katrinaarbogast@gmail.com>
2023-09-18 15:55:02 -0600

    added print statements to lightning cleaning script

--- lightning_fire_cleaning.py
+++ lightning_fire_cleaning.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 lightning_fires = pd.read_csv('./DirtyData/Wildfire_Data/lightining_fires_kaggle/US_Lightning_Forest_Fires.csv')
 
+print(lightning_fires.info())
+
 # removing the index columns and the FIPS since they had a lot of missing data
 lightning_fires = lightning_fires.drop(columns=["Unnamed: 0", "index", "FIPS_CODE", "FIPS_NAME"])
 lightning_fires["Fire_Date"] = pd.to_datetime(lightning_fires["Fire_Date"])
