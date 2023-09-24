824a3afd171fc742e7b09a83a5c86d955cbeb064
Katrina Arbogast <katrinaarbogast@gmail.com>
2023-09-24 11:02:39 -0600

    Condensed the cleaning of lightning fires and month fires into one script

---
+++ data_cleaning_fire.py
@@ -0,0 +1,94 @@
+import sqlite3
+import pandas as pd
+
+path_clean = './CleanData'
+fires_monthly_folder_path = './DirtyData/Wildfire_Data/By_month_fires'
+lightning_fires_path_dirty = './DirtyData/Wildfire_Data/lightining_fires_kaggle/US_Lightning_Forest_Fires.csv'
+
+
+def main():
+    
+    ## Fires Burned Monthly Datasets
+    print("\n\n ---------- Fires Burned Monthly Datasets ---------- \n")
+    fires_df_dict = read_in_fires_monthly(fires_monthly_folder_path)
+    us_fires_burn_monthly = clean_month_fire_data(fires_df_dict)
+    save_clean_to_csv(us_fires_burn_monthly, path_clean, "us_fires_burn_monthly.csv")
+    
+    ## Lightning Dataset
+    print("\n\n ---------- Lightning Fires Dataset ---------- \n")
+    lightning_df = lightning_fires_cleaning(lightning_fires_path_dirty)
+    save_clean_to_csv(lightning_df, path_clean, "lightning_wildfires_clean.csv")
+
+
+def read_in_fires_monthly(fires_monthly_folder_path):
+    ## Per Month Wildfire Data
+    df_month_dict = {
+        "January" : pd.read_csv(f'{fires_monthly_folder_path}/US_Wildfires_January.csv', skiprows=1),
+        "February": pd.read_csv(f'{fires_monthly_folder_path}/US_Wildfires_February.csv', skiprows=1),
+        "March" : pd.read_csv(f'{fires_monthly_folder_path}/US_Wildfires_March.csv', skiprows=1),
+        "April" : pd.read_csv(f'{fires_monthly_folder_path}/US_Wildfires_April.csv', skiprows=1),
+        "May" : pd.read_csv(f'{fires_monthly_folder_path}/US_Wildfires_May.csv', skiprows=1),
+        "June" : pd.read_csv(f'{fires_monthly_folder_path}/US_Wildfires_June.csv', skiprows=1),
+        "July" : pd.read_csv(f'{fires_monthly_folder_path}/US_Wildfires_July.csv', skiprows=1),
+        "August" : pd.read_csv(f'{fires_monthly_folder_path}/US_Wildfires_August.csv', skiprows=1),
+        "September" : pd.read_csv(f'{fires_monthly_folder_path}/US_Wildfires_September.csv', skiprows=1),
+        "October" : pd.read_csv(f'{fires_monthly_folder_path}/US_Wildfires_October.csv', skiprows=1),
+        "November" : pd.read_csv(f'{fires_monthly_folder_path}/US_Wildfires_November.csv', skiprows=1),
+        "December" : pd.read_csv(f'{fires_monthly_folder_path}/US_Wildfires_December.csv', skiprows=1)
+    }
+    return df_month_dict
+
+def clean_month_fire_data(df_dict):
+    for key, value in df_dict.items():
+        value["Month"] = key
+    
+    df = pd.concat(df_dict.values(), axis=0)
+
+    df = df.rename(columns={"Date":"Year", 
+                            "Number of Fires" : "Number_of_Fires", 
+                            "Acres Burned per Fire" : "Acres_Burned_per_Fire", 
+                            "Acres Burned" : "Acres_Burned"})
+    df["Year"] = df["Year"].astype("object")
+    df2 = df.copy()
+    
+    # Removing 2023 because this data is not complete for all months yet - Project being done in 2023
+    df2 = df2[df2["Year"] != 2023]
+    year_list = list(df2['Year'].unique())
+                     
+    print(f'\nUnique Year in Full dataframe: \n{year_list}\n')
+    print(f"\n{df2.info()}")
+    
+    return df2
+
+    
+def lightning_fires_cleaning(input_dir):
+
+    lightning_fires = pd.read_csv(input_dir)
+
+    lightning_fires = lightning_fires.drop(columns=["Unnamed: 0", "index", "FIPS_CODE", "FIPS_NAME"])
+    lightning_fires["Fire_Date"] = pd.to_datetime(lightning_fires["Fire_Date"])
+    lightning_fires = lightning_fires.dropna()
+
+    print(lightning_fires.info())
+    
+    return lightning_fires
+
+
+def database_connection(relative_path, table_name):
+    cnx = sqlite3.connect(relative_path)
+    df = pd.read_sql_query(f"SELECT * FROM {table_name}", cnx)
+
+    cnx.close()
+    
+    return df
+
+
+def save_clean_to_csv(df, output_dir, filename):
+
+    df.to_csv(f"{output_dir}/{filename}", index=False)
+
+    
+
+# DO NOT REMOVE!!!
+if __name__ == "__main__":
+    main()
---
+++ paths_for_remaining_datasets.py
@@ -0,0 +1,38 @@
+
+# # Wildfire Data
+# us_wildfires_2mil = database_connection('./Wildfire_Data/US_2mil_wildfires_kaggle/FPA_FOD_20221014.sqlite', 'Fires')
+# ca_wildfires = pd.read_csv('./Wildfire_Data/ca_wildfires_kaggle/California_Fire_Incidents.csv')
+# or_weather_wildfires = pd.read_csv('./Wildfire_Data/oregon_wildfire_kaggle/Wildfire_Weather_Merged_new.csv',
+#                                    dtype={
+#                                         'Cause_Comments' : 'str',
+#                                         'DistFireNumber' : 'str'
+#                                     })
+# lightning_fires = pd.read_csv('./Wildfire_Data/lightining_fires_kaggle/US_Lightning_Forest_Fires.csv')
+
+
+# # Climate Change Kaggle Data
+# global_temp_by_city = pd.read_csv('./Weather_drought_data/climate_change_kaggle/GlobalLandTemperaturesByCity.csv')
+# global_temp_by_country = pd.read_csv('./Weather_drought_data/climate_change_kaggle/GlobalLandTemperaturesByCountry.csv')
+# global_temp_by_major_city = pd.read_csv('./Weather_drought_data/climate_change_kaggle/GlobalLandTemperaturesByMajorCity.csv')
+# global_temp_by_state = pd.read_csv('./Weather_drought_data/climate_change_kaggle/GlobalLandTemperaturesByState.csv')
+# global_temp_avg = pd.read_csv('./Weather_drought_data/climate_change_kaggle/GlobalTemperatures.csv')
+
+
+# # Hourly Weather Data
+# weather_city_attributes = pd.read_csv('./Weather_drought_data/hour_weather_data/city_attributes.csv')
+# weeather_humidity = pd.read_csv('./Weather_drought_data/hour_weather_data/humidity.csv')
+# weather_pressure = pd.read_csv('./Weather_drought_data/hour_weather_data/pressure.csv')
+# weather_temperature = pd.read_csv('./Weather_drought_data/hour_weather_data/temperature.csv')
+# weather_description = pd.read_csv('./Weather_drought_data/hour_weather_data/weather_description.csv')
+# weather_wind_direction = pd.read_csv('./Weather_drought_data/hour_weather_data/wind_direction.csv')
+# weather_wind_speed = pd.read_csv('./Weather_drought_data/hour_weather_data/wind_speed.csv')
+
+
+# # Drought Montior Data
+# dm_national_total_area = pd.read_csv('./Weather_drought_data/drought_monitor_data/dm_export_20000101_20230101-national-totalArea-catergorical.csv')
+# dm_regional_total_area = pd.read_csv('./Weather_drought_data/drought_monitor_data/dm_export_20000101_20230101-region-totalArea-catergorical.csv')
+# dm_state_total_area = pd.read_csv('./Weather_drought_data/drought_monitor_data/dm_export_20000101_20230101-state-totalArea-catergorical.csv')
+# dm_RDEWS_total_area = pd.read_csv('./Weather_drought_data/drought_monitor_data/dm_export_20000101_20230101_RDEWS_totalArea.csv')
+
+
+
