21266543960abda92bd35f05b8c3de2db4d83ab1
Katrina Arbogast <katrinaarbogast@gmail.com>
2023-10-25 18:27:47 -0600

    Added oregon weather values as one of the datasets to be modeled

--- naive_bayes.py
+++ naive_bayes.py
@@ -52,6 +52,9 @@ def main():
     news_headlines_df_selected = news_headlines_df[news_headlines_df['LABEL'].isin(["wildfire", "weather"])]
     news_headlines_df_selected2 = news_headlines_df[news_headlines_df['LABEL'].isin(["burn", "weather"])]
     news_headlines_df2 = set_new_generic_label(news_headlines_df, "LABEL")
+    
+    or_weather_wildfires_df["FireDuration_hrs"] = or_weather_wildfires_df["FireDuration_hrs"].apply(lambda x: 0 if x <= 0 else x)
+    or_weather_wildfires_df[["tmax", "tmin", "tavg", "prcp"]] = or_weather_wildfires_df[["tmax", "tmin", "tavg", "prcp"]].apply(lambda x: (x-x.min())/ (x.max() - x.min()))  
 
     
     print("\n\n ---------- Selecting Train and Test Data ---------- \n")
@@ -65,7 +68,21 @@ def main():
     
     or_cause_train_labels, or_cause_train_df, or_cause_test_labels, or_cause_test_df = setup_train_test_data(or_weather_wildfires_comments_vector_df, "GeneralCause")
     or_specific_train_labels, or_specific_train_df, or_specific_test_labels, or_specific_test_df = setup_train_test_data(or_weather_wildfires_specific_vector_df, "GeneralCause")
+    or_train_labels, or_train_df, or_test_labels, or_test_df = setup_train_test_data(or_weather_wildfires_df, label_col="Size_class", cols_of_interst_plus_label=["Size_class","tmax", "tmin", "tavg", "prcp", "FireDuration_hrs"])
+
+    print("\n\n ---------- Naive Bayes ---------- \n")
+    multinomial_naive_bayes(fires_monthly_train_labels, fires_monthly_train_df, fires_monthly_test_labels, fires_monthly_test_df, us_fires_burn_monthly_filename)
+    multinomial_naive_bayes(fires_season_train_labels, fires_season_train_df, fires_season_test_labels, fires_season_test_df, f"{us_fires_burn_monthly_filename}_season")
     
+    multinomial_naive_bayes(news_train_labels, news_train_df, news_test_labels, news_test_df, news_headlines_vector_filename)
+    multinomial_naive_bayes(news2_train_labels, news2_train_df, news2_test_labels, news2_test_df, f"{news_headlines_vector_filename}_2")
+    multinomial_naive_bayes(news_select_train_labels, news_select_train_df, news_select_test_labels, news_select_test_df, f"{news_headlines_vector_filename}_wildfires_weather")
+    multinomial_naive_bayes(news_select2_train_labels, news_select2_train_df, news_select2_test_labels, news_select2_test_df, f"{news_headlines_vector_filename}_burn_weather")
+    
+    multinomial_naive_bayes(or_cause_train_labels, or_cause_train_df, or_cause_test_labels, or_cause_test_df, or_weather_wildfires_comments_vector_filename)
+    multinomial_naive_bayes(or_specific_train_labels, or_specific_train_df, or_specific_test_labels, or_specific_test_df, or_weather_wildfires_specific_vector_filename)
+    multinomial_naive_bayes(or_train_labels, or_train_df, or_test_labels, or_test_df, or_weather_wildfires_filename)
+
     
 def set_fire_months(df, month_col):
     
@@ -112,11 +129,11 @@ def setup_train_test_data(df, label_col, cols_of_interst_plus_label=None, test_s
 def multinomial_naive_bayes(train_labels, train_df, test_labels, test_df, filename):
 
     #Create the modeler
-    MyModelNB= MultinomialNB()
+    MyModelNB = MultinomialNB()
 
-    NB1=MyModelNB.fit(train_df, train_labels)
-    naive_pred = MyModelNB.predict(train_df)
-    print(np.round(MyModelNB.predict_proba(train_df),2))
+    MyModelNB.fit(train_df, train_labels)
+    naive_pred = MyModelNB.predict(test_df)
+    # print(np.round(MyModelNB.predict_proba(test_df),2))
 
     print("\nThe prediction from NB is:")
     print(naive_pred)
@@ -126,11 +143,10 @@ def multinomial_naive_bayes(train_labels, train_df, test_labels, test_df, filena
     score = accuracy_score(test_labels, naive_pred)
     print(f"\nThe accurary is for {filename} is : {score}\n")
 
-    ## Show the confusion matrix
+    # Show the confusion matrix
     cm = confusion_matrix(test_labels, naive_pred)
     print(f"\nThe confusion matrix for {filename} is:")
     print(cm)
-
 
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=MyModelNB.classes_)
     plt.figure(figsize=(18, 15))
@@ -138,11 +154,8 @@ def multinomial_naive_bayes(train_labels, train_df, test_labels, test_df, filena
     plt.xticks(rotation=45, ha='right')
     plt.title(f"Confusion Matrix\n - {filename} -") 
     plt.tight_layout()
-    plt.savefig(f"./CreatedVisuals/DecisionTree/{filename}_cm.png")
+    plt.savefig(f"./CreatedVisuals/NaiveBayes/{filename}_cm.png")
     plt.close()
-
-
-
 
 # DO NOT REMOVE!!!
 if __name__ == "__main__":
